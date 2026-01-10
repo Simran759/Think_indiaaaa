48de01a39c74da7e970edc56086f34c6991de547
simran <vanshikab458@gmail.com>
2026-01-10 21:01:26 +0530

    add files

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config({ path: '../../.env' });
+// dotenv.config({ path: '../../.env' });
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-console.log("API_BASE_URL:", process.env.REACT_APP_API_URL);
+const API_BASE_URL = "https://think-indiaa.vercel.app/"
+// console.log("API_BASE_URL:", process.env.REACT_APP_API_URL);
 
 export default API_BASE_URL;
