d096d5cd568609496102e200902935262ab9cd75
simran <vanshikab458@gmail.com>
2026-01-10 23:32:05 +0530

    change in url

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-
+// const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://think-indiaa.vercel.app";
 console.log("API_BASE_URL:", API_BASE_URL);
 
 export default API_BASE_URL;
